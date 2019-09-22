e779a2e3bb387dca0d498d597fb489f21c404162
valAizcorbe <valeria.aizcorbe@hotmail.com>
2019-09-21 20:41:30 -0600

    finished

--- classes.js
+++ classes.js
@@ -29,7 +29,20 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee{
+  constructor (first_name, last_name, email, age){
+  this.first_name = first_name,
+  this.last_name = last_name,
+  this.email = email,
+  this.age = age
+  } 
+  
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
+
+const valEmployee = new Employee('Valeria', 'Aizcorbe', 'email@email.com', 24)
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +60,18 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee{
+  constructor(first_name, last_name, email, age){
+    super (first_name, last_name, email, age)
+   this.reports = [] }
+  hire(employee){
+    this.reports.push(employee)
+  }
+  fire(index){
+    this.reports.splice(index, 1)
+  }
+}
+
 
 
 ////////// PROBLEM 3 //////////
@@ -71,9 +95,38 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
-
-
+class ProgressiveManager extends Manager{
+  constructor(first_name, last_name, email, age,reports){
+    super(first_name, last_name, email, age,reports)
+    this.title = 'Not a manager',
+    this.bonus = 0
+  }
+  hiredOrFired(){
+    if (this.reports.length === 0) {
+      this.title = 'Not a manager'
+    } else if (this.reports.length >= 1 && this.reports.length <= 3){
+      this.title = 'Barely Manager'
+    } else if (this.reports.length >= 4 && this.reports.length <= 10){
+      this.title = 'Mostly Manager'
+    } else if (this.reports.length >=11 && this.reports.length <= 50){
+      this.title = 'Manager'
+    } else if (this.reports.length >= 51 && this.reports.length <= 100){
+      this.title =  'Manager Plus'
+    }else if (this.reports.length >= 101){
+      this.title =  'Bestest Manager'
+    }
+  }
+  
+  hire(employee){
+    this.reports.push(employee)
+    this.hiredOrFired()
+  }
+  fire(index){
+    this.reports.splice(index, 1)
+    this.hiredOrFired()
+    this.bonus += 100;
+  }
+}
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
